969e916a506abd87801bb534e048578cd1dd668c
Ripudaman Teja <ripu23@Ripus-MacBook-Air.local>
2018-10-14 10:34:00 -0700

    making generic method to convert json to java

--- src/main/java/chartconstellation/app/Test.java
+++ src/main/java/chartconstellation/app/Test.java
@@ -1,48 +1,21 @@
 package chartconstellation.app;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import chartconstellation.app.engine.ObjectMappingUtil;
 import chartconstellation.app.entities.Chart;
 
 public class Test {
 
+
+	
 	public static void main(String[] args) {
-		Test test = new Test();
-		test.run();
-
-	}
-
-	private void run() {
-		ObjectMapper mapper = new ObjectMapper();
-
+		ObjectMappingUtil util = new ObjectMappingUtil();
+		List<Chart> list = new ArrayList<>();
 		String path = "/Users/ripu/Documents/projects/Workspace_DV/chartconstellation/src/main/resources/Data_Images";
-		try {
-			Stream<Path> paths = Files.walk(Paths.get(path));
-			paths.forEach(filePath -> {
-				if (Files.isRegularFile(filePath)) {
-
-					try {
-						
-							System.out.println(filePath.getFileName());
-							if(filePath.getFileName().toString().contains("json")) {
-								Chart chart = mapper.readValue(filePath.toFile(), Chart.class);
-								System.out.println(chart);
-							}
-						
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		util.convertToObject(path, Chart.class, list);
+		System.out.println(list.size());
 
 	}
 
---
+++ src/main/java/chartconstellation/app/engine/ObjectMappingUtil.java
@@ -0,0 +1,43 @@
+package chartconstellation.app.engine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class ObjectMappingUtil {
+
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	@SuppressWarnings("unchecked")
+	public void convertToObject(String path, Class classType, List list) {
+		try {
+			Stream<Path> paths = Files.walk(Paths.get(path));
+			paths.forEach(filePath -> {
+				if (Files.isRegularFile(filePath)) {
+
+					try {
+
+						System.out.println(filePath.getFileName());
+						if (filePath.getFileName().toString().contains("json")) {
+							Object chart = mapper.readValue(filePath.toFile(), classType);
+							list.add(chart);
+						}
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
