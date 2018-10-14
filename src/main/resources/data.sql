9370d164490d34b52d34a8802c7a6b99bb62a80b
Ripudaman Teja <ripu23@Ripus-MacBook-Air.local>
2018-10-13 18:05:21 -0700

    Deleting some files

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO movie(movie_Name, print_type, price) VALUES ('Star Wars Episode IV','DVD',20),
-('Star Wars Episode V','DVD',20),
-('Star Wars Episode VI','DVD',20),
-('Star Wars Episode IV','Blu-Ray',25),
-('Star Wars Episode V','Blu-Ray',25),
-('Star Wars Episode VI','Blu-Ray',25);
