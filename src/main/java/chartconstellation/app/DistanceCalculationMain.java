e6491871660b588a016824b998488593eae0fd74
Ripudaman Teja <ripu23@Ripus-MacBook-Air.local>
2018-10-23 14:13:54 -0700

    adding node module to calculate description distance

--- src/main/java/chartconstellation/app/DistanceCalculationMain.java
+++ src/main/java/chartconstellation/app/DistanceCalculationMain.java
@@ -2,16 +2,16 @@ package chartconstellation.app;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import chartconstellation.app.engine.ObjectMappingUtil;
 import chartconstellation.app.entities.Chart;
 
-public class Test {
+public class DistanceCalculationMain {
 
 
 	
 	public static void main(String[] args) {
 		ObjectMappingUtil util = new ObjectMappingUtil();
+		
 		List<Chart> list = new ArrayList<>();
 		String path = "/Users/ripu/Documents/projects/Workspace_DV/chartconstellation/src/main/resources/Data_Images";
 		util.convertToObject(path, Chart.class, list);
---
+++ src/main/resources/node/server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var stringSimilarity = require('string-similarity');
+
+var config = require('../Data_Images/ArunSankar1.json');
+
+console.log(config.description);
