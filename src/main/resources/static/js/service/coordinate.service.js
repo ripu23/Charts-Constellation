6d50c1296ede44da4ca717af8f019d8af4486334
Ripudaman Teja <ripu23@ripus-air.3521.dhcp.asu.edu>
2018-10-25 10:15:54 -0700

    adding apis

---
+++ src/main/resources/static/js/service/coordinate.service.js
@@ -0,0 +1,12 @@
+'use strict'
+var app =angular.module("mainApp");
+
+app.factory("CoordinateService", function($http){
+  return{
+    getCoordinates: function(){
+      return $http.get("/coordinate/getCoordinates", function(response){
+        return response.data;
+      })
+    }
+  }
+})
