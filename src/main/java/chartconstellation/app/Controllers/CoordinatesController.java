ed1240ebabcf657eedfea2e5310aff3d43ec8827
manoj yaramsetty <manojys1994@gmail.com>
2018-10-26 14:55:24 -0700

    adding Get Coordinates API

--- src/main/java/chartconstellation/app/Controllers/CoordinatesController.java
+++ src/main/java/chartconstellation/app/Controllers/CoordinatesController.java
@@ -1,6 +1,7 @@
 package chartconstellation.app.Controllers;
 
 import chartconstellation.app.appconfiguration.Configuration;
+import chartconstellation.app.entities.response.IdCoordinates;
 import chartconstellation.app.util.CoordinatesUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @RestController
 public class CoordinatesController {
@@ -19,12 +22,12 @@ public class CoordinatesController {
     CoordinatesUtil coordinatesUtil;
 
     @RequestMapping(value="/getCoordinates", method= RequestMethod.GET)
-    public ModelAndView login(@RequestParam("descWeight") Double descWeight,
+    public List<IdCoordinates> coordinates(@RequestParam("descWeight") Double descWeight,
                               @RequestParam("attrWeight") Double attrWeight,
                               @RequestParam("chartEncodingWeight") Double chartEncodingWeight) {
 
-        coordinatesUtil.calculateCoordinates(descWeight, attrWeight, chartEncodingWeight);
+        List<IdCoordinates> coordinatesList = coordinatesUtil.calculateCoordinates(descWeight, attrWeight, chartEncodingWeight);
 
-        return null;
+        return coordinatesList;
     }
 }
--- src/main/java/chartconstellation/app/entities/response/Point.java
+++ src/main/java/chartconstellation/app/entities/response/Point.java
@@ -5,6 +5,11 @@ public class Point {
     private Double x;
     private Double y;
 
+    public Point(Double x, Double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public Double getX() {
         return x;
     }
