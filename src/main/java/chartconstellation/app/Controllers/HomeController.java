afc7803a5d8eb5f030ba00ac1e5708e5b2c9c693
Ripudaman Teja <ripu23@Ripus-MacBook-Air.local>
2018-10-31 17:03:49 -0700

    Backend code refactoring

--- src/main/java/chartconstellation/app/Controllers/HomeController.java
+++ src/main/java/chartconstellation/app/Controllers/HomeController.java
@@ -1,46 +1,21 @@
 package chartconstellation.app.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import chartconstellation.app.entities.User;
-import chartconstellation.app.service.UserService;
-
 @RestController
 public class HomeController {
-
-	@Autowired
-	private UserService userService;
 	
 	@RequestMapping("/")
 	public ModelAndView loginRender() {
 		return new ModelAndView("index.html");
 	}
 	
-	@RequestMapping("/register")
-	public @ResponseBody User register(@RequestBody User user) {
-		return userService.save(user); 
-	}
 	
 	@RequestMapping(value="/home")
 	public ModelAndView home(){
 		return new ModelAndView("index.html");
 	}
 	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public ModelAndView login(@RequestParam("userName") String userName, @RequestParam("password") String password) {
-		
-		User fetchedUser = userService.getUser(new User(userName, password));
-		
-		if(fetchedUser != null) {
-			return new ModelAndView("redirect:/home");
-		}
-		return null;
-	}
 }
