4420c2d43aad4b8edb31af7a5f0e9a2d3fef5e4a
manoj yaramsetty <manojys1994@gmail.com>
2018-11-17 20:19:56 -0700

    new changes

--- src/main/java/chartconstellation/app/entities/response/DataCoverageResponse.java
+++ src/main/java/chartconstellation/app/entities/response/DataCoverageResponse.java
@@ -1,10 +1,32 @@
 package chartconstellation.app.entities.response;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 public class DataCoverageResponse {
 
     private HashMap<String, Integer> attributesMap;
+
+    private Set<String> attributeKeys;
+
+    private HashMap<String, List<String>> attributeOccurenceMap;
+
+    public Set<String> getAttributeKeys() {
+        return attributeKeys;
+    }
+
+    public void setAttributeKeys(Set<String> attributeKeys) {
+        this.attributeKeys = attributeKeys;
+    }
+
+    public HashMap<String, List<String>> getAttributeOccurenceMap() {
+        return attributeOccurenceMap;
+    }
+
+    public void setAttributeOccurenceMap(HashMap<String, List<String>> attributeOccurenceMap) {
+        this.attributeOccurenceMap = attributeOccurenceMap;
+    }
 
     public HashMap<String, Integer> getAttributesMap() {
         return attributesMap;
