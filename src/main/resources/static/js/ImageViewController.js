42a6f6f8db6b429e703ec924dcf8a63aaca7aed8
Ripudaman Teja <ripu23@ripus-air.3521.dhcp.asu.edu>
2018-11-27 14:33:49 -0700

    added more details to image view

--- src/main/resources/static/js/ImageViewController.js
+++ src/main/resources/static/js/ImageViewController.js
@@ -3,12 +3,22 @@
 var app = angular.module("mainApp");
 app.controller("ImageViewController", ['$scope',
  'ClusterService',
- function($scope, ClusterService) {
+ 'ShareData',
+ function($scope, ClusterService, ShareData) {
   console.log('reached ImageViewController');
   $scope.images = ClusterService.getImageUrl().images;
   $scope.constantUrl = ClusterService.getImageUrl().url;
+  let allDetails = ShareData.data.allDetails;
+  function makeMap(){
+    _.forEach($scope.images, function(image, key){
+      _.forEach(allDetails[image.img.substring(0,image.img.indexOf('.'))], function(data, idx){
+          image[idx] = data;
+      });
+    });
+  }
   $scope.$on('datasetChanged', function(event, data){
     $scope.images = ClusterService.getImageUrl().images;
     $scope.constantUrl = ClusterService.getImageUrl().url;
   });
+  makeMap();
 }]);
